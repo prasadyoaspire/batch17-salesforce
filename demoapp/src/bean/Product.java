32980f11fdd7ccfdd6a84f5985bbd718861bc33e
Prasad <prasad.yoaspire@gmail.com>
2023-04-18 15:40:07 +0530

    Product.java updated

--- demoapp/src/bean/Product.java
+++ demoapp/src/bean/Product.java
@@ -1,9 +1,13 @@
 package bean;
 
-public class Product {
+public class Product implements Comparable<Product> {
     private int productId;
     private String productName;
     private double productPrice;
+
+    public Product() {
+
+    }
     public Product(int productId,String productName,double productPrice) {
         this.productId = productId;
         this.productName = productName;
@@ -26,5 +30,42 @@ public class Product {
     }
     public void setProductPrice(double productPrice) {
         this.productPrice = productPrice;
-    }    
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + productId;
+        result = prime * result + ((productName == null) ? 0 : productName.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(productPrice);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (productId != other.productId)
+            return false;
+        if (productName == null) {
+            if (other.productName != null)
+                return false;
+        } else if (!productName.equals(other.productName))
+            return false;
+        if (Double.doubleToLongBits(productPrice) != Double.doubleToLongBits(other.productPrice))
+            return false;
+        return true;
+    }
+    @Override
+    public int compareTo(Product o) {
+      //int result = this.productId - o.getProductId();
+      int result = this.productName.compareTo(o.getProductName());
+      return result;
+    }     
 }
